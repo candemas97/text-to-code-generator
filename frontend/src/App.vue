016f2d895d62e7fd766f35c549c35a4a5de5cedb
cargar_github <candemas97@hotmail.com>
2023-11-08 22:38:40 -0500

    frontend v2 😉

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,25 @@
-<script lang="ts">
-
-</script>
-
 <template>
-    <h1>El generador Texto Código</h1>
+  <h1 class="titulo">El generador Texto Código</h1>
+  <InputQuery :prop1="varApp" />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import InputQuery from "./components/InputQuery.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { InputQuery },
+  data: function () {
+    return {
+        varApp: "pene"
+    };
+  },
+});
+</script>
+
 <style scoped>
+/* h1 {
+  color: "red";
+} */
 </style>
